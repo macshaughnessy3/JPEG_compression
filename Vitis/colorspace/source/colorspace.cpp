3d1f73b2521c9e334c4029f2b4172b647ac0ff72
Mac Shaughnessy <tmacshaughnessy@gmail.com>
2024-02-12 01:37:40 -0500

    typo

--- Vitis/colorspace/source/colorspace.cpp
+++ Vitis/colorspace/source/colorspace.cpp
@@ -22,9 +22,7 @@ void color_convert_2(video_stream& stream_in, video_stream& stream_out) {
 	ap_int<16> Cb = 128+((-((r<<5)+(r<<2)+(r<<1))-((g<<6)+(g<<3)+(g<<1))+((b<<7)-(b<<4)))>>8);
 	ap_int<16> Cr = 128+(((r<<7)-(r<<4)-((g<<6)+(g<<5)-(g<<1))-((b<<4)+(b<<1)))>>8);
 
-	curr_pixel.data.range(
-
-7,0) = Y;
+	curr_pixel.data.range(7,0) = Y;
 	curr_pixel.data.range(15,8) = Cb;
 	curr_pixel.data.range(31,16) = Cr;
 
@@ -34,47 +32,47 @@ void color_convert_2(video_stream& stream_in, video_stream& stream_out) {
 
 ////#define N 500;
 //
-//void downsample(video_stream& stream_in_48, video_stream& stream_out_48) {
-//	#pragma HLS INTERFACE ap_ctrl_none port=return
-//	#pragma HLS INTERFACE axis port=stream_in_48 register
-//	#pragma HLS INTERFACE axis port=stream_out_48 register
-//
-//	auto N=500;
-//	ap_uint<64> image_block[N][N];
-//	ap_uint<64> downsample_block[N/2][N/2];
-////	#pragma HLS ARRAY_PARTITION variable=image_block type=complete dim=0
-////	#pragma HLS ARRAY_PARTITION variable=downsample_block type=complete dim=0
-//
-//	pixel curr_pixel;
-//	pixel curr_pixel_out;
-//	curr_pixel.keep=-1;
-//	curr_pixel_out.keep=-1;
-//	curr_pixel_out.last=0;
-//
-//
-//	read_image:
-//		for(auto i=0; i<N; i++){
-//			for(auto j=0; j<N; j++){
-//				curr_pixel = stream_in_48.read();
-//				image_block[i][j]=curr_pixel.data;
-//			}
-//		}
-//	downsample:
-//		for(auto i=0;i<N/2;i++){
-//			for(auto j=0; j<N/2; j++){
-//				downsample_block[i][j].range(15,0)=(image_block[i*2][j*2].range(15,0)+image_block[i*2+1][j*2+1].range(15,0)+image_block[i*2+1][j*2].range(15,0)+image_block[i*2][j*2+1].range(15,0))/4;
-//				downsample_block[i][j].range(31,16)=(image_block[i*2][j*2].range(31,16)+image_block[i*2+1][j*2+1].range(31,16)+image_block[i*2+1][j*2].range(31,16)+image_block[i*2][j*2+1].range(31,16))/4;
-//				downsample_block[i][j].range(47,32)=(image_block[i*2][j*2].range(47,32)+image_block[i*2+1][j*2+1].range(47,32)+image_block[i*2+1][j*2].range(47,32)+image_block[i*2][j*2+1].range(47,32))/4;
-//			}
-//		}
-//	write_image:
-//		for(auto i=0;i<N/2;i++){
-//			for(auto j=0; j<N/2; j++){
-//				curr_pixel_out.data = downsample_block[i][j];
-//				stream_out_48.write(curr_pixel_out);
-//				if (i==N/2-1 && j>N/2-3){
-//					curr_pixel_out.last=1;
-//				}
-//			}
-//		}
-//}
+void downsample(video_stream& stream_in, video_stream& stream_out) {
+	#pragma HLS INTERFACE ap_ctrl_none port=return
+	#pragma HLS INTERFACE axis port=stream_in_48 register
+	#pragma HLS INTERFACE axis port=stream_out_48 register
+
+	auto N=500;
+	ap_uint<64> image_block[N][N];
+	ap_uint<64> downsample_block[N/2][N/2];
+//	#pragma HLS ARRAY_PARTITION variable=image_block type=complete dim=0
+//	#pragma HLS ARRAY_PARTITION variable=downsample_block type=complete dim=0
+
+	pixel curr_pixel;
+	pixel curr_pixel_out;
+	curr_pixel.keep=-1;
+	curr_pixel_out.keep=-1;
+	curr_pixel_out.last=0;
+
+
+	read_image:
+		for(auto i=0; i<N; i++){
+			for(auto j=0; j<N; j++){
+				curr_pixel = stream_in_48.read();
+				image_block[i][j]=curr_pixel.data;
+			}
+		}
+	downsample:
+		for(auto i=0;i<N/2;i++){
+			for(auto j=0; j<N/2; j++){
+				downsample_block[i][j].range(7,0)=(image_block[i*2][j*2].range(7,0)+image_block[i*2+1][j*2+1].range(7,0)+image_block[i*2+1][j*2].range(15,0)+image_block[i*2][j*2+1].range(7,0))>>2;
+				downsample_block[i][j].range(15,8)=(image_block[i*2][j*2].range(15,8)+image_block[i*2+1][j*2+1].range(15,8)+image_block[i*2+1][j*2].range(15,8)+image_block[i*2][j*2+1].range(15,8))>>2;
+				downsample_block[i][j].range(31,16)=(image_block[i*2][j*2].range(31,16)+image_block[i*2+1][j*2+1].range(31,16)+image_block[i*2+1][j*2].range(31,16)+image_block[i*2][j*2+1].range(31,16))>>2;
+			}
+		}
+	write_image:
+		for(auto i=0;i<N/2;i++){
+			for(auto j=0; j<N/2; j++){
+				curr_pixel_out.data = downsample_block[i][j];
+				stream_out_48.write(curr_pixel_out);
+				if (i==N/2-1 && j>N/2-3){
+					curr_pixel_out.last=1;
+				}
+			}
+		}
+}
